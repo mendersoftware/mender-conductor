0ba280379f2493d3ff4b3167053d06eafe371e89
Piotr Przybylak <piotr.przybylak@gmail.com>
2017-11-21 10:53:44 +0100

    Email sender worker - add extra config
    Issues: MEN-1568
    
    Changelog: None
    
    Signed-off-by: Piotr Przybylak <piotr.przybylak@gmail.com>

--- workers/send_email/main.py
+++ workers/send_email/main.py
@@ -1,10 +1,10 @@
 import logging
-import smtplib
 import traceback
 
 from conductor.ConductorWorker import ConductorWorker
 
 import config
+import send_email
 
 logging.basicConfig()
 log = logging.getLogger('conductor-workers.send_email')
@@ -13,24 +13,11 @@ log.setLevel(logging.INFO)
 conf = config.get_config()
 
 
-def send_email(recipient, title, body):
-    if conf.demo == "true":
-        log.info("Demo mode on. Intercepted send email request with recipient: {}\nTitle: {}\nBody:\n{}".format(recipient, title, body))
-        return
-    message = 'To: {}\nSubject: {}\n\n{}'.format(recipient, title, body)
-    server = smtplib.SMTP(conf.smtpaddress)
-    server.ehlo()
-    server.starttls()
-    server.login(conf.smtplogin, conf.smtppassword)
-    server.sendmail(conf.emailsender, recipient, message)
-    server.quit()
-
-
 def send_email_task(task):
     try:
-        send_email(task['inputData']['email'], task['inputData']['title'], task['inputData']['body'])
+        send_email.send_email(task['inputData']['email'], task['inputData']['title'], task['inputData']['body'], conf)
 
-        # always return this well formed response- status, output, logs
+        # always return this well formed response - status, output, logs
         return {'status': 'COMPLETED',
                 'output': {},
                 'logs': []}
