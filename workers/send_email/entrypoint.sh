a2da66607290d408b151ed0fd175c855b4220abc
Krzysztof Jaskiewicz <krzysiekjaskiewicz@gmail.com>
2018-02-27 00:11:43 +0100

    add send email worker with Dockerfile and entrypoint
    changelog: none
    
    Signed-off-by: Krzysztof Jaskiewicz <krzysiekjaskiewicz@gmail.com>

---
+++ workers/send_email/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/ash
+
+CONDUCTOR=${CONDUCTOR="http://mender-conductor:8080"}
+
+echo "-- conductor API is at ${CONDUCTOR}"
+
+# wait for conductor
+tries=0
+up=0
+while [ "$tries" -lt 20 ]; do
+    if ! curl -f "${CONDUCTOR}/api/metadata/taskdefs" > /dev/null 2>&1 ; then
+        tries=$((tries + 1))
+        echo "-- $(date) waiting for conductor, attempt ${tries}"
+        sleep 5
+    else
+        up=1
+        break
+    fi
+done
+
+if [ "$up" -ne 1 ]; then
+    echo "-- max attempts reached, giving up"
+    exit 1
+fi
+
+python3 /usr/bin/workers/send_email/main.py
+exit 1
