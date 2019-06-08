b1970474cf6362e621adedc84115e540795694c4
Oleksandr Miliukov <oleksandr.miliukov@northern.tech>
2019-06-08 12:25:48 +0200

    Timestamp added to send_email worker
    Changelog: Title
    Signed-off-by: Oleksandr Miliukov <oleksandr.miliukov@northern.tech>
    (cherry picked from commit 5fcec1010ab3c6c3856f9c9bbde2ae79d771d7a6)

--- workers/send_email/entrypoint.sh
+++ workers/send_email/entrypoint.sh
@@ -2,7 +2,11 @@
 
 CONDUCTOR=${CONDUCTOR="http://mender-conductor:8080"}
 
-echo "-- conductor API is at ${CONDUCTOR}"
+ts() {
+    echo -ne "[$(python -c "from datetime import datetime; now=datetime.now(); string_i_want=('%04d-%02d-%02d %02d:%02d:%02d,%03d'%(now.year,now.month,now.day,now.hour,now.minute,now.second,now.microsecond))[:-3]; print( string_i_want )")]"
+}
+
+echo "$(ts) [INFO    ] conductor API is at ${CONDUCTOR}"
 
 # wait for conductor
 tries=0
@@ -10,7 +14,7 @@ up=0
 while [ "$tries" -lt 20 ]; do
     if ! curl -f "${CONDUCTOR}/api/metadata/taskdefs" > /dev/null 2>&1 ; then
         tries=$((tries + 1))
-        echo "-- $(date) waiting for conductor, attempt ${tries}"
+        echo "$(ts) [INFO    ] waiting for conductor, attempt ${tries}"
         sleep 5
     else
         up=1
@@ -19,9 +23,9 @@ while [ "$tries" -lt 20 ]; do
 done
 
 if [ "$up" -ne 1 ]; then
-    echo "-- max attempts reached, giving up"
+    echo "$(ts) [ERROR   ] max attempts reached, giving up"
     exit 1
 fi
 
-python -u /usr/bin/workers/send_email/main.py
+/usr/local/bin/python2.7 -u /usr/bin/workers/send_email/main.py
 exit 1
--- workers/send_email/worker/send_email.py
+++ workers/send_email/worker/send_email.py
@@ -1,12 +1,20 @@
 import logging
 import smtplib
-
-logging.basicConfig()
-log = logging.getLogger('conductor-workers.send_email')
-log.setLevel(logging.INFO)
+import traceback
+import config
 
 
-def send_email(recipient, title, body, conf):
+conf = config.get_config()
+
+logs_format='[%(asctime)s] [%(levelname)-8s] %(message)s'
+if conf.debug:
+    logging.basicConfig(format=logs_format, level=logging.DEBUG)
+else:
+    logging.basicConfig(format=logs_format, level=logging.INFO)
+log = logging.getLogger()
+
+
+def send_email(recipient, title, body):
     if conf.demo == "true":
         log.info(
             "Demo mode on. Intercepted send email request with recipient: {}\nTitle: {}\nBody:\n{}".format(recipient,
@@ -16,17 +24,22 @@ def send_email(recipient, title, body, conf):
     message = 'From: {}\nTo: {}\nSubject: {}\n\n{}'.format(conf.emailsender, recipient, title, body)
     log.debug("Sending email message: {}".format(message))
 
-    server = smtplib.SMTP(conf.smtpaddress)
+    try:
+        server = smtplib.SMTP(conf.smtpaddress)
 
-    if conf.smtpssl == "true":
-        server.starttls()
-    if conf.smtplogin and len(conf.smtplogin) > 0:
-        log.debug("Logging in to SMTP account..")
-        server.login(conf.smtplogin, conf.smtppassword)
+        if conf.smtpssl == "true":
+            server.starttls()
+        if conf.smtplogin and len(conf.smtplogin) > 0:
+            log.debug("Logging in to SMTP account..")
+            server.login(conf.smtplogin, conf.smtppassword)
 
-    log.debug("Sending email..")
-    server.sendmail(conf.emailsender, recipient, message.encode("UTF8"), ['SMTPUTF8'])
-    log.debug("Email sent!")
+        log.debug("Sending email..")
+        server.sendmail(conf.emailsender, recipient, message.encode("UTF8"), ['SMTPUTF8'])
+        log.debug("Email sent!")
 
-    server.quit()
-    log.debug("Done.")
+        server.quit()
+        log.debug("Done.")
+
+    except:
+        log.error("Failed to send message to '%s'" % recipient)
+        log.fatal(traceback.format_exc())
